9a9127ebb31ae0be5ab266c732d1188fe67dcf69
Unknown <dani.lazaro@hotmail.com>
2017-10-25 18:40:47 +0100

    Collab

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,6 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+name= "Daniela"
+food= "tacos"
+sentence = "My name is {} and I like {}".format(name, food)
+print(sentence)
