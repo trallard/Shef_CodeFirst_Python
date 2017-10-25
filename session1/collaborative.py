6f71d0f53265421ac3f35cb2cbe2c33fded9d7a9
Unknown <pragyachaube@gmail.com>
2017-10-25 18:19:18 +0100

    Made changes to collaborative.py
    Session1

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,10 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+
+# add name and favourite food
+name = "Pragya"
+food = "daal-chawal"
+
+# print
+letsprint = "I am {} and I love to eat {}".format(name , food)
+print(letsprint)
