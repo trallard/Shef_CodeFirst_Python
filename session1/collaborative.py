e81db503d216ddb7cba7c06af7ff787a2050ccfb
Unknown <miaceveslopez1@sheffield.ac.uk>
2017-10-25 18:34:56 +0100

    Adding collaborative path

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,7 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+name = 'Maureen'
+fav_food = 'Thai & Mexican food'
+
+description = "Hello! My name is {} and my favorite food is {}.".format(name, fav_food)
+print(description)
