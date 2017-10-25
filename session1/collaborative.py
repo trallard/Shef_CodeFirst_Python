b92ea6c7c4a9777f2640eabbde8fec7144fb0097
Unknown <hfmottram1@sheffield.ac.uk>
2017-10-25 18:29:41 +0100

    Collaborative exercise session 1

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,6 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+my_name = 'Hannah'
+my_favourite_food = 'chocolate'
+description = "My name is {} and I like {}".format(my_name, my_favourite_food)
+print (description)
