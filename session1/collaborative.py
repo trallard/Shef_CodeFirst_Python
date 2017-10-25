b7a02bbff4b418a6d105c0dcb5d0b39706e27001
Unknown <hfmottram1@sheffield.ac.uk>
2017-10-25 18:48:47 +0100

    Cookies

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,6 +1,6 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
 my_name = 'Hannah'
-my_favourite_food = 'chocolate'
+my_favourite_food = 'chocolate cookies'
 description = "My name is {} and I like {}".format(my_name, my_favourite_food)
 print (description)
