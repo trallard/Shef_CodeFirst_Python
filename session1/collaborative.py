c6b55a495f21936fc31261ee2a3825b9c828cdad
Hope <hopejohanna10@gmail.com>
2017-10-25 18:30:38 +0100

    Collaborative ex session1

--- session1/collaborative.py
+++ session1/collaborative.py
@@ -1,2 +1,5 @@
 # Hi there welcome to the collaborative exercise!
 # Start typing your answers below
+name = 'Hope'
+favourite_food = 'pizza'
+description = 'My name is {} and my favourite food is {}.'. format (name, favourite_food)
