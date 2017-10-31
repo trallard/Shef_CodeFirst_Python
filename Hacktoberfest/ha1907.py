82112c9a43bb1a0a8b8c4f587808c38ca964ed11
Hope <hopejohanna10@gmail.com>
2017-10-31 21:06:55 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Hacktoberfest/ha1907.py
@@ -0,0 +1,5 @@
+
+name = input("What is your name?")
+order = input ("What would you like?")
+milk = input ("Would you like whole milk or almond milk?")
+print(f"So, {name}, you would like {order} with {milk}, that will be £ 3.00 please.")
