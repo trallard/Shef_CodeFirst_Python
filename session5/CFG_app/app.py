95a3d4147b0d0a4e1ce39a27fc918ed130f9ccd2
Tania Allard <t.sanchez@sheffield.ac.uk>
2017-11-13 20:37:59 +0000

    Change icons in index

--- session5/CFG_app/app.py
+++ session5/CFG_app/app.py
@@ -13,17 +13,28 @@ def index():
 
 @app.route("/twitter")
 def twitter_handler():
+    """ This should display a form where the users introduce the terms
+    they want to look for in Twitter"""
     return render_template("twitter.html")
 
 @app.route("/twitter_search", methods = ['POST'])
 def twitter_search_app():
+    """When the search is submitted from the previous page,
+    this function passes the form data to our python scripts
+    and uses them to perform the query. It will then return the tweets
+    and display them in /tweets_show"""
     query = request.form['query']
-
     tweets = collect_tweets(query)
-
 
     return render_template('tweets_show.html', search_string=query,
                            tweets =tweets)
+
+
+@app.route("/spotify")
+def spotify_handler():
+    """ This should display a form where the users introduce the terms
+    they want to look for in Twitter"""
+    return render_template("spotify.html")
 
 
 # "debug=True" causes Flask to automatically refresh upon any changes you
